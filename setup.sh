197a5504e0fa6ae6d4802895b32116439201d480
Michael Kwok <michael.kwok00@gmail.com>
2019-02-01 10:42:58 -0700

    Finally got custom keybinds working

--- setup.sh
+++ setup.sh
@@ -54,6 +54,7 @@ git config --global user.name "Michael Kwok"
 git config --global user.email "michael.kwok00@gmail.com"
 
 # Custom keybinds
+dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/binding "'<Primary><Alt>t'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/name "'Terminal'"
 dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/command "'gnome-terminal'"
