e3ad6543afafa30bf45532b36ee201e8ba07986b
Michael Kwok <michael.kwok00@gmail.com>
2020-08-24 10:32:35 -0600

    more updates

--- setup.sh
+++ setup.sh
@@ -8,19 +8,21 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+# Alacritty
+sudo dnf copr enable pschyska/alacritty 
 # End repo setup
 
 # Start package install
 
 # DNF
-sudo dnf -y update
+sudo dnf check-update
 
-sudo dnf -y install zsh # Make shit look nice
-sudo dnf -y install adobe-source-code-pro-fonts adobe-source-sans-pro-fonts powerline-fonts adobe-source-serif-pro-font
-sudo dnf -y install virt-manager radare2 dnf-utils neovim code # Frequently used programs
-sudo dnf -y install boost-devel clang lldb llvm libcxx-devel gcc code valgrind# Dev stuff
+sudo dnf -y install zsh alacritty gnome-tweak-tool gtk-murrine-engine # Make shit look nice
+sudo dnf -y install virt-manager radare2 dnf-utils neovim code piper qemu # Frequently used programs
+sudo dnf -y install boost-devel clang lldb llvm libcxx-devel gcc code valgrind # Dev stuff
 
 # Flathub
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.discordapp.Discord
 flatpak install flathub com.spotify.Client
 
