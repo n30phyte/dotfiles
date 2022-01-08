0f36642cd6968206421b5b69eb9120c23a9b2e62
Michael Kwok <michael.kwok00@gmail.com>
2022-01-08 13:05:30 -0700

    Update setup shell script

--- setup.sh
+++ setup.sh
@@ -8,24 +8,27 @@ sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-rele
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
-# Alacritty
-sudo dnf copr enable pschyska/alacritty 
+# GH CLI
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh
+
 # End repo setup
 
 # Start package install
-
 # DNF
 sudo dnf check-update
 
-sudo dnf -y install zsh alacritty gnome-tweak-tool gtk-murrine-engine # Make shit look nice
-sudo dnf -y install virt-manager radare2 dnf-utils neovim code piper qemu # Frequently used programs
-sudo dnf -y install boost-devel clang lldb llvm libcxx-devel gcc code valgrind # Dev stuff
+sudo dnf -y install zsh alacritty gnome-tweak-tool google-noto-serif-fonts google-noto-sans-fonts cascadia-fonts-all
+sudo dnf -y install virt-manager radare2 neovim code qemu
+sudo dnf -y install boost-devel clang lldb llvm gcc code valgrind
 
 # Flathub
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub com.bitwarden.desktop
 flatpak install flathub com.discordapp.Discord
+flatpak install flathub com.getmailspring.Mailspring
 flatpak install flathub com.spotify.Client
-
+flatpak install flathub org.signal.Signal
 # End package install
 
 # Git configs
@@ -33,11 +36,10 @@ git config --global user.name "Michael Kwok"
 git config --global user.email "michael.kwok00@gmail.com"
 
 # Install antigen for zsh
-curl -L git.io/antigen > ~/.antigen.zsh
+curl -L git.io/antigen >~/.antigen.zsh
 
 # Link rcfiles
 ln -s $PWD/zsh/zshrc ~/.zshrc
-ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
 
 ln -s $PWD/mako ~/.config/
 ln -s $PWD/rofi ~/.config/
@@ -47,3 +49,6 @@ ln -s $PWD/waybar ~/.config/
 
 # Make directories
 mkdir ~/Workspace
+
+# Install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
